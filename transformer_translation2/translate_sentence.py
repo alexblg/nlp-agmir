e4114be440fdbcc9fe869fd3962b735c78a68926
alexblg <alexandre.boulenger@outlook.com>
2020-06-11 14:53:30 +0800

    renamed folder

---
+++ transformer_translation2/translate_sentence.py
@@ -0,0 +1,15 @@
+import pickle
+from einops import rearrange
+
+#import spacy
+import torch
+
+def gen_nopeek_mask(length):
+    mask = rearrange(torch.triu(torch.ones(length, length)) == 1, 'h w -> w h')
+    mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+
+    return mask
+
+
+if __name__ == "__main__":
+    main()
