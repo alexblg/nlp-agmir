39c6805d3a11c270f9c39e63a1f0a04ed1f646ee
alexblg <alexandre.boulenger@outlook.com>
2020-06-11 14:36:56 +0800

    initial commit

--- perf.py
+++
@@ -1,7 +0,0 @@
-from nltk.translate.bleu_score import sentence_bleu
-import numpy as np
-
-def get_av_bleu(pred_output):
-    return np.mean([
-        sentence_bleu([r[1].split(' ')], r[2].split(' ')
-                     ) for r in pred_output])
